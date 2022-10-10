01b6227dd5a9bd7e47c34e2c7cfc19507e94abab
josefggx <josefgg58@gmail.com>
2022-10-09 21:16:05 -0500

    ✨ add: Basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/';
+import { Navbar } from './components/Navbar/Navbar';
 import { AboutPage } from './pages/AboutPage';
-import { HomePage } from './pages/HomePage';
+import { HomeContactPage } from './pages/HomeContactPage';
+import { HomePage } from './pages/HomePage/HomePage';
 import { ProjectsPage } from './pages/ProjectsPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <br />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        {/* <Route path="/contact" element={<HomeContactPage />} /> */}
 
         {/* <Route path="/*" element={<HomePage />} /> */}
         <Route path="*/" element={<Navigate to="/" />} />

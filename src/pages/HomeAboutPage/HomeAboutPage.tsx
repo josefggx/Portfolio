9bb310ea9ba6fe08eaf3c0a549cf23ded05aefb0
josefggx <josefgg58@gmail.com>
2022-10-12 16:56:37 -0500

    ✨ add: Home About Page

---
+++ src/pages/HomeAboutPage/HomeAboutPage.tsx
@@ -0,0 +1,9 @@
+import './HomeAboutPage.scss';
+
+export const HomeAboutPage = () => {
+  return (
+    <section id="about" className="section about">
+      <h1>HomeAboutPage</h1>
+    </section>
+  );
+};

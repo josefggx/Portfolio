d59cd2ef6fa4da377e37b338c829317e2d0f80ef
josefggx <josefgg58@gmail.com>
2022-10-21 23:13:05 -0500

    💄 add: Styling and structure for about home page

--- src/pages/HomeAboutPage/HomeAboutPage.tsx
+++ src/pages/HomeAboutPage/HomeAboutPage.tsx
@@ -1,9 +1,33 @@
+import { TechStack } from '../../components/TechStack/TechStack';
 import './HomeAboutPage.scss';
 
 export const HomeAboutPage = () => {
   return (
     <section id="about" className="section about">
-      <h1>HomeAboutPage</h1>
+      <div className="about__container">
+        <h1 className="about__title">
+          About me<span>.</span>
+        </h1>
+        <div className="about__content">
+          <p className="about__text">
+            Amet fugiat adipisicing proident id commodo aute voluptate ad aute
+            aliquip dolore aliqua. Lorem eu ea eiusmod in est quis non sint
+            ipsum anim aliqua anim. Fugiat adipisicing cupidatat amet voluptate
+            ex voluptate ullamco commodo officia sunt excepteur labore dolore.
+            Enim eu pariatur amet qui.
+            <br />
+            <br /> Amet fugiat adipisicing proident id commodo aute voluptate ad
+            aute aliquip dolore aliqua. Lorem eu ea eiusmod in est quis non sint
+            ipsum anim aliqua anim. Fugiat adipisicing cupidatat amet voluptate
+            ex voluptate ullamco commodo officia sunt excepteur labore dolore.
+            Enim eu pariatur amet qui.
+            <br />
+            <br />
+            <TechStack />
+          </p>
+          <div className="about__image"></div>
+        </div>
+      </div>
     </section>
   );
 };

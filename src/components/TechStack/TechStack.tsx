bd3eda161b4a5f6655735f545324a0dba37af3ba
josefggx <josefgg58@gmail.com>
2022-10-25 22:03:42 -0500

    💄 update: Updated the structure and styling

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -3,24 +3,20 @@ import './TechStack.scss';
 export const TechStack = () => {
   return (
     <>
-      <h2 className="techstack__title">Tech Stack:</h2>
+      <h3 className="techstack__title">Tech Stack:</h3>
       <ul className="techstack__list">
         <li className="techstack__item">HTML5</li>
-        <li className="techstack__item">CSS3</li>
-        <li className="techstack__item">SASS</li>
-        <li className="techstack__item">Tailwind</li>
+        <li className="techstack__item">CSS / SASS</li>
         <li className="techstack__item">JavaScript</li>
         <li className="techstack__item">TypeScript</li>
-        <li className="techstack__item">Node.js</li>
-        <li className="techstack__item">Jest</li>
         <li className="techstack__item">ReactJS</li>
         <li className="techstack__item">React Native</li>
-        <li className="techstack__item">Redux</li>
-        <li className="techstack__item">Next.js</li>
+        <li className="techstack__item">Node.js</li>
         <li className="techstack__item">MongoDB</li>
+        <li className="techstack__item">Next.js</li>
+        <li className="techstack__item">Redux</li>
+        <li className="techstack__item">Tailwind</li>
         <li className="techstack__item">Git & Github</li>
-        <li className="techstack__item">Linux</li>
-        <li className="techstack__item">Figma</li>
       </ul>
     </>
   );

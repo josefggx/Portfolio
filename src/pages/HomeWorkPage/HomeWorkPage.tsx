dc4603035e64c72f3f449c0ee893bbe31d6f45da
josefggx <josefgg58@gmail.com>
2022-10-25 22:06:13 -0500

    ✨ add: Home work page basic structure

---
+++ src/pages/HomeWorkPage/HomeWorkPage.tsx
@@ -0,0 +1,10 @@
+import { Title } from '../../components/Title/Title';
+import './HomeWorkPage.scss'
+
+export const HomeWorkPage = () => {
+  return (
+    <section id="work" className="section work">
+      <Title title="Latest Work" />
+    </section>
+  );
+}

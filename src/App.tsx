2146c74601ace898bde9a97b0aae76feb83cfe34
josefggx <josefgg58@gmail.com>
2022-10-25 22:02:41 -0500

    🐛 fix: corrected styling and routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Cursor } from './components/Cursor/Cursor';
 import { Navbar } from './components/Navbar/Navbar';
-import { AboutPage } from './pages/AboutPage';
-import { HomeContactPage } from './pages/HomeContactPage';
 import { HomePage } from './pages/HomePage/HomePage';
-import { ProjectsPage } from './pages/ProjectsPage';
 
 function App() {
   return (
@@ -13,10 +9,6 @@ function App() {
       {/* <Cursor /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        {/* <Route path="/contact" element={<HomeContactPage />} /> */}
-
         {/* <Route path="/*" element={<HomePage />} /> */}
         <Route path="*/" element={<Navigate to="/" />} />
       </Routes>

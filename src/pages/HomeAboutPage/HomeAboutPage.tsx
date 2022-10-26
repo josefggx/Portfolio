a9e488d4520439b513763a0c011df38cc9662c5c
josefggx <josefgg58@gmail.com>
2022-10-25 22:05:09 -0500

    🚧 add: Links and button for cv

--- src/pages/HomeAboutPage/HomeAboutPage.tsx
+++ src/pages/HomeAboutPage/HomeAboutPage.tsx
@@ -1,31 +1,50 @@
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button/Button';
 import { TechStack } from '../../components/TechStack/TechStack';
+import { Title } from '../../components/Title/Title';
 import './HomeAboutPage.scss';
 
 export const HomeAboutPage = () => {
   return (
     <section id="about" className="section about">
       <div className="about__container">
-        <h1 className="about__title">
-          About me<span>.</span>
-        </h1>
+        <Title title="About me" />
         <div className="about__content">
           <div className="about__text">
             Amet fugiat adipisicing proident id commodo aute voluptate ad aute
             aliquip dolore aliqua. Lorem eu ea eiusmod in est quis non sint
             ipsum anim aliqua anim. Fugiat adipisicing cupidatat amet voluptate
             ex voluptate ullamco commodo officia sunt excepteur labore dolore.
-            Enim eu pariatur amet qui.
-            <br />
-            <br /> Amet fugiat adipisicing proident id commodo aute voluptate ad
-            aute aliquip dolore aliqua. Lorem eu ea eiusmod in est quis non sint
-            ipsum anim aliqua anim. Fugiat adipisicing cupidatat amet voluptate
-            ex voluptate ullamco commodo officia sunt excepteur labore dolore.
-            Enim eu pariatur amet qui.
+            Enim eu pariatur amet qui. Fugiat adipisicing cupidatat amet
+            voluptate ex voluptate ullamco commodo officia sunt excepteur labore
+            dolore. Enim eu pariatur amet qui. Fugiat adipisicing cupidatat amet
+            voluptate ex voluptate ullamco commodo officia sunt excepteur labore
+            dolore. Enim eu pariatur amet qui.
             <br />
             <br />
             <TechStack />
           </div>
-          <div className="about__image"></div>
+          <div className="about__info">
+            <div className="about__image"></div>
+            <Button text="Resume" />
+            <div className="about__links">
+              <a href="http://www.github.com" target="_blank">
+                <span>
+                  <i className="fa-brands fa-github"></i>
+                </span>
+              </a>
+              <a href="http://www.linkedin.com" target="_blank">
+                <span>
+                  <i className="fa-brands fa-linkedin-in"></i>
+                </span>
+              </a>
+              <a href="http://www.linkedin.com" target="_blank">
+                <span>
+                  <i className="fa-regular fa-envelope"></i>
+                </span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>

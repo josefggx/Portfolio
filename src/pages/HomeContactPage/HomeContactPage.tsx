b678cbaf5ce35a4d47d2456476d2f75cc6dae91a
josefggx <josefgg58@gmail.com>
2022-10-25 22:05:26 -0500

    ✨ add: Basic Contact Page structure

---
+++ src/pages/HomeContactPage/HomeContactPage.tsx
@@ -0,0 +1,10 @@
+import { Title } from '../../components/Title/Title';
+import './HomeContactPage.scss'
+
+export const HomeContactPage = () => {
+  return (
+    <section id="contact" className="section contact">
+      <Title title="Contact me" />
+    </section>
+  );
+}

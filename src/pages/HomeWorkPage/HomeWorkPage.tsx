5c48adb26fb15ed4342a9717ba3bba5b3ee840e9
josefggx <josefgg58@gmail.com>
2022-11-03 21:03:20 -0500

    💄 update: Styling and structure

--- src/pages/HomeWorkPage/HomeWorkPage.tsx
+++ src/pages/HomeWorkPage/HomeWorkPage.tsx
@@ -1,10 +1,15 @@
+
+import { ProjectCard } from '../../components/ProjectCard/ProjectCard';
 import { Title } from '../../components/Title/Title';
-import './HomeWorkPage.scss'
+import './HomeWorkPage.scss';
 
 export const HomeWorkPage = () => {
   return (
     <section id="work" className="section work">
-      <Title title="Latest Work" />
+      <div className="work__container">
+        <Title title="Latest Work" />
+        <ProjectCard />
+      </div>
     </section>
   );
-}
+};

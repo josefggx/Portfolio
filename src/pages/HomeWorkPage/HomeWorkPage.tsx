657afe8bbd206a88e5fa509bc1d2fc1e50f5ff4c
josefggx <josefgg58@gmail.com>
2022-11-09 23:51:47 -0500

    ✨ add: Button to see more projects

--- src/pages/HomeWorkPage/HomeWorkPage.tsx
+++ src/pages/HomeWorkPage/HomeWorkPage.tsx
@@ -1,4 +1,4 @@
-
+import { Button } from '../../components/Button/Button';
 import { ProjectCard } from '../../components/ProjectCard/ProjectCard';
 import { Title } from '../../components/Title/Title';
 import './HomeWorkPage.scss';
@@ -9,6 +9,12 @@ export const HomeWorkPage = () => {
       <div className="work__container">
         <Title title="Latest Work" />
         <ProjectCard />
+        <Button
+          onClick={() => {
+            window.open('https://github.com/josefggx', '_blank');
+          }}
+          text="Github"
+        />
       </div>
     </section>
   );

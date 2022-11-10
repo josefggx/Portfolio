00025839dfb3fef0476d73ddb21c6b1e6f687ed1
josefggx <josefgg58@gmail.com>
2022-11-09 23:51:24 -0500

    ✨ add: Project data info, summary

--- src/utils/projectsData.ts
+++ src/utils/projectsData.ts
@@ -16,13 +16,12 @@ export const projectsData: projectsDataTypes = [
     id: '2',
     type: 'work',
     image:
-      'https://res.cloudinary.com/josefggx-cloud/image/upload/v1667612430/Portfolio/301436521_131834572902915_514818785872804505_n_k8vjil.png',
+      'https://res.cloudinary.com/josefggx-cloud/image/upload/v1668034891/Portfolio/Project1_u6ruzu.png',
     tech: ['react native', 'typescript', 'redux', 'node'],
     title: 'AceMate',
     summary:
-      'Non fugiat ad esse sit exercitation ex. Adipisicing eiusmod quis consequat ea deserunt duis aliqua do est. Labore adipisicing voluptate dolore proident excepteur est esse et. Est adipisicing aliqua officia commodo qui reprehenderit fugiat irure voluptate cupidatat ullamco ipsum aliquip. Excepteur consectetur sint fugiat ipsum consectetur amet id incididunt non quis labore deserunt quis ut.',
-    description:
-      'Ea fugiat nisi enim eiusmod commodo eiusmod sit veniam commodo sint nulla cupidatat. Eiusmod officia dolor irure magna consectetur officia officia nulla sunt irure. Mollit aliqua officia mollit consectetur exercitation. Culpa enim et proident est magna id culpa enim. Sit voluptate culpa nisi nisi nostrud nostrud tempor elit ipsum in dolor. Ad Lorem ut officia in nisi amet enim dolore elit voluptate ipsum officia. Ut nostrud duis deserunt id. Proident ex sunt reprehenderit irure cillum aliquip aute aliqua dolor. Quis ut laborum ea qui quis deserunt labore enim eu. Eu culpa id commodo aute reprehenderit sit fugiat. Sit sint culpa consequat anim veniam.',
+      "AceMate is a virtual tennis platform that allows athletes and trainers to create matches and make court reservations quickly and effectively. This is the last project I've been working on in ZanaCode, developed using React Native.",
+    description: '',
     instagram: 'https://www.instagram.com/acemate.col/',
     livepage: '/public/acemate.png',
   },
@@ -30,13 +29,12 @@ export const projectsData: projectsDataTypes = [
     id: '1',
     type: 'personal',
     image:
-      'https://res.cloudinary.com/josefggx-cloud/image/upload/v1667612506/Portfolio/Data_architect_INESDI_1_larerg.jpg',
+      'https://res.cloudinary.com/josefggx-cloud/image/upload/v1668034897/Portfolio/Project2_cm94v3.jpg',
     tech: ['react', 'typescript', 'sass'],
     title: 'Personal Portfolio',
     summary:
-      'Non fugiat ad esse sit exercitation ex. Adipisicing eiusmod quis consequat ea deserunt duis aliqua do est. Labore adipisicing voluptate dolore proident excepteur est esse et. Est adipisicing aliqua officia commodo qui reprehenderit fugiat irure voluptate cupidatat ullamco ipsum aliquip. Excepteur consectetur sint fugiat ipsum consectetur amet id incididunt non quis labore deserunt quis ut.',
-    description:
-      'Ea fugiat nisi enim eiusmod commodo eiusmod sit veniam commodo sint nulla cupidatat. Eiusmod officia dolor irure magna consectetur officia officia nulla sunt irure. Mollit aliqua officia mollit consectetur exercitation. Culpa enim et proident est magna id culpa enim. Sit voluptate culpa nisi nisi nostrud nostrud tempor elit ipsum in dolor. Ad Lorem ut officia in nisi amet enim dolore elit voluptate ipsum officia. Ut nostrud duis deserunt id. Proident ex sunt reprehenderit irure cillum aliquip aute aliqua dolor. Quis ut laborum ea qui quis deserunt labore enim eu. Eu culpa id commodo aute reprehenderit sit fugiat. Sit sint culpa consequat anim veniam.',
+      'My portfolio started with the idea of ​​being a simple project that would serve to show my other projects. But, while I was doing it, it became a project in itself. I like to think it tells a bit about my story, style, and passion.',
+    description: '',
     github: 'https://github.com/josefggx/Portfolio',
     livepage: 'https://github.com/josefggx/Portfolio',
   },

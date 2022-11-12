2ec0899e67a893298c63ed3c2101b44fc01d3cbf
josefggx <josefgg58@gmail.com>
2022-11-12 00:12:26 -0500

    ✨ add: UseEffect to customize loading behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar/Navbar';
 import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
+  useEffect(() => {
+    if (window.location.hash) {
+      const section = window.location.hash.slice(1);
+      document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, []);
+
   return (
     <div className="App">
       <Navbar />

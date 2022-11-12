c13ae195ac28f77a6c37003f075783684a4a8b1b
josefggx <josefgg58@gmail.com>
2022-11-12 01:19:04 -0500

    ✨ add: Footer component structure & styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,32 @@
-import './Footer.scss'
+import './Footer.scss';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="footer__links">
+        <a href="http://www.github.com" target="_blank">
+          <span>
+            <i className="fa-brands fa-github"></i>
+          </span>
+        </a>
+        <a href="http://www.linkedin.com" target="_blank">
+          <span>
+            <i className="fa-brands fa-linkedin-in"></i>
+          </span>
+        </a>
+        <a href="http://www.linkedin.com" target="_blank">
+          <span>
+            <i className="fa-regular fa-envelope"></i>
+          </span>
+        </a>
+        <a href="http://www.linkedin.com" target="_blank">
+          <span>
+            <i className="fa-regular fa-note-sticky"></i>
+          </span>
+        </a>
+      </div>
+      <p id="footer-text">José Gutiérrez @2022</p>
+    </footer>
+  );
+};
+

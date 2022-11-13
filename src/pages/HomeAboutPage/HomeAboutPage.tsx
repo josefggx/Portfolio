59828d309232f9863155cdfb18ba13d250b97551
josefggx <josefgg58@gmail.com>
2022-11-13 01:32:24 -0500

    ✨ add: Wrote the about me profile text

--- src/pages/HomeAboutPage/HomeAboutPage.tsx
+++ src/pages/HomeAboutPage/HomeAboutPage.tsx
@@ -17,15 +17,19 @@ export const HomeAboutPage = () => {
         <Title title="About me" />
         <div className="about__content">
           <div className="about__text">
-            Amet fugiat adipisicing proident id commodo aute voluptate ad aute
-            aliquip dolore aliqua. Lorem eu ea eiusmod in est quis non sint
-            ipsum anim aliqua anim. Fugiat adipisicing cupidatat amet voluptate
-            ex voluptate ullamco commodo officia sunt excepteur labore dolore.
-            Enim eu pariatur amet qui. Fugiat adipisicing cupidatat amet
-            voluptate ex voluptate ullamco commodo officia sunt excepteur labore
-            dolore. Enim eu pariatur amet qui. Fugiat adipisicing cupidatat amet
-            voluptate ex voluptate ullamco commodo officia sunt excepteur labore
-            dolore. Enim eu pariatur amet qui.
+            Hello! My name is José, and I'm a passionate Software developer from
+            Colombia.
+            <br />
+            I'm in love with what I do since it combines creativity, logic, and
+            problem-solving, and there's always something new to learn. Also,
+            I'm passionate about human sciences and try to be a better person,
+            friend, and colleague every day.
+            <br />
+            I'm committed to a philosophy of lifelong learning since I enjoy the
+            feeling of discovering and improving. I've worked in management,
+            finance, and technology. These experiences give me new perspectives
+            that help me adapt to projects, understand the business logic and be a
+            versatile and efficient developer.
             <br />
             <br />
             <h2 className="item__title">Tech Stack</h2>

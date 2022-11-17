582fbd7b2be5a312e9bb191403334827899c20ab
josefggx <josefgg58@gmail.com>
2022-11-17 16:22:33 -0500

    🐛 fix: Corrected mobile styles

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -3,20 +3,20 @@ import './TechStack.scss';
 export const TechStack = () => {
   return (
     <>
-      <ul className="techstack__list">
-        <li className="techstack__item">HTML5</li>
-        <li className="techstack__item">CSS / Sass</li>
-        <li className="techstack__item">JavaScript</li>
-        <li className="techstack__item">TypeScript</li>
-        <li className="techstack__item">ReactJS</li>
-        <li className="techstack__item">React Native</li>
-        <li className="techstack__item">Node.js</li>
-        <li className="techstack__item">MongoDB</li>
-        <li className="techstack__item">Next.js</li>
-        <li className="techstack__item">Redux</li>
-        <li className="techstack__item">Tailwind</li>
-        <li className="techstack__item">Git & Github</li>
-      </ul>
+      <div className="techstack__list">
+        <div className="techstack__item">HTML5</div>
+        <div className="techstack__item">JavaScript</div>
+        <div className="techstack__item">CSS / Sass</div>
+        <div className="techstack__item">TypeScript</div>
+        <div className="techstack__item">ReactJS</div>
+        <div className="techstack__item">React Native</div>
+        <div className="techstack__item">Node.js</div>
+        <div className="techstack__item">MongoDB</div>
+        <div className="techstack__item">Next.js</div>
+        <div className="techstack__item">Redux</div>
+        <div className="techstack__item">Tailwind</div>
+        <div className="techstack__item">Git & Github</div>
+      </div>
     </>
   );
 };
